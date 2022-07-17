8be5493c3ed39cce0ddaa77734a0ab37595f95e0
Satoru <you@example.com>
2022-07-17 10:17:24 +0900

    feat: minor update

--- src/extension.ts
+++ src/extension.ts
@@ -2,38 +2,34 @@ import * as vscode from "vscode";
 import axios from "axios";
 
 export function activate(context: vscode.ExtensionContext) {
-  const triggerCharacter: any = vscode.workspace
-    .getConfiguration("vscode-completions-hi")
-    .get("triggerCharacter");
+  const conf: any = vscode.workspace.getConfiguration("vscode-completions-hi");
+  const triggerCharacter: any = conf.get("triggerCharacter");
 
   //パネルを作成する
   const provider1 = vscode.languages.registerCompletionItemProvider(
     ["plaintext", "xml"],
     {
       async provideCompletionItems(document, position) {
-        const api = vscode.workspace
-          .getConfiguration("vscode-completions-hi")
-          .get("api");
+        const api = conf.get("api");
         // get all text until the `position` and check if it reads `console.`
         // and if so then complete if `log`, `warn`, and `error`
-        let linePrefix: string = document
+        const linePrefix: string = document
           .lineAt(position)
           .text.substr(0, position.character);
 
         //後方の文字列
-        let lineSuffix: string = document
+        const lineSuffix: string = document
           .lineAt(position)
           .text.substr(position.character);
 
         //タグで分割
-        const tags = [" ", "。", "#", '"', ">", "<", "."];
+        const tags: string[] = [" ", "。", "#", '"', ">", "<", "."];
 
         //タグで分割された文字列
         let modifiedLinePrefix = linePrefix;
         let modifiedLineSuffix = lineSuffix;
         for (const tag of tags) {
           //後方
-
           let spl = modifiedLineSuffix.split(tag);
           modifiedLineSuffix = spl[0];
 
@@ -42,18 +38,15 @@ export function activate(context: vscode.ExtensionContext) {
           }
 
           //前方
-
           spl = modifiedLinePrefix.split(tag);
           modifiedLinePrefix = spl[spl.length - 1];
         }
 
-        let modifiedLinePrefix2 = modifiedLinePrefix;
+        let queryText = modifiedLinePrefix.replace(triggerCharacter, "");
 
-        const queryText = modifiedLinePrefix.replace(triggerCharacter, "");
+        queryText = queryText + "[MASK]" + modifiedLineSuffix;
 
-        const text = queryText + "[MASK]" + modifiedLineSuffix;
-
-        const url = api + encodeURIComponent(text);
+        const url = api + encodeURIComponent(queryText);
 
         const results = [];
 
@@ -65,7 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
             const index = i + 1;
             const p = item.token_str;
 
-            const label = `${modifiedLinePrefix2}${p} (${(
+            const label = `${modifiedLinePrefix}${p} (${(
               item.score * 100
             ).toFixed(1)}%)`;
 
@@ -80,12 +73,8 @@ export function activate(context: vscode.ExtensionContext) {
             commandCompletion.sortText = String(index);
             commandCompletion.keepWhitespace = true;
             commandCompletion.insertText = insertText;
-            commandCompletion.detail = text;
-            if (
-              vscode.workspace
-                .getConfiguration("vscode-completions-hi")
-                .get("re-trigger")
-            ) {
+            commandCompletion.detail = queryText;
+            if (conf.get("re-trigger")) {
               commandCompletion.command = {
                 command: "editor.action.triggerSuggest",
                 title: "Re-trigger completions...",
